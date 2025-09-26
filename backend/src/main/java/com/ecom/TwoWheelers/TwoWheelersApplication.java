2063e3dc4348ac4ca01c5576f4e9764ddc60751f
sri pavithra <sripavithrapayyavula2005@gmail.com>
2025-09-26 15:54:47 +0530

    springboot setup is done!

---
+++ backend/src/main/java/com/ecom/TwoWheelers/TwoWheelersApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.TwoWheelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoWheelersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoWheelersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecom/TwoWheelers/TwoWheelersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.TwoWheelers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoWheelersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
